15dec6c894845cdceacb63beb35fc5740048b935
Markus Dobler <mail@markusdobler.de>
2017-08-24 22:18:40 +0200

    Skip checks ...

--- backpack-dispatcher.py
+++ backpack-dispatcher.py
@@ -2,22 +2,15 @@
 
 import os
 BACKPACK_DOTDIR = os.path.join(os.path.expanduser('~'), ".backpack")
-LOGFILE = os.path.join(BACKPACK_DOTDIR, "backpack.log")
+LOGFILE = os.path.join(BACKPACK_DOTDIR, "log", "backpack.log")
 DATEFMT = "%Y-%m-%d %H:%M:%S"
 SUCCESS_URL = 'https://mado.phoenix.uberspace.de/backpack/log/success/%s'
 
 
 import logging, subprocess, json, urllib2
-from contextlib import contextmanager
+from logging.handlers import RotatingFileHandler
 from datetime import datetime, timedelta
-
-
-def setup_logging(path=LOGFILE):
-    from logging.handlers import RotatingFileHandler
-    handler = RotatingFileHandler(path, maxBytes=5000, backupCount=5)
-    handler.setFormatter(logging.Formatter('%(levelname).1s %(asctime)s: %(message)s', datefmt=DATEFMT))
-    logging.root.addHandler(handler)
-    logging.root.setLevel(logging.DEBUG)
+from trbs_pid import PidFile, PidFileError
 
 
 def touch(fname, times=None):
@@ -28,26 +21,8 @@ class SkipExecution(Exception):
     pass
 
 
-@contextmanager
-def logged_call(jobname):
-    try:
-        logging.info("%s start" % jobname)
-        yield
-        touch(success_timestamp_file(jobname))
-        logging.info("%s success" % jobname)
-        try:
-            urllib2.urlopen(SUCCESS_URL % jobname, timeout=10)
-        except:
-            pass
-    except SkipExecution as ex:
-        logging.warn("%s skip: %s" % (jobname, ex))
-    except Exception as ex:
-        logging.error("%s fail: %s" % (jobname, ex))
-        raise
-
-
 def success_timestamp_file(jobname):
-    return os.path.join(BACKPACK_DOTDIR, "success.%s" % jobname)
+    return os.path.join(BACKPACK_DOTDIR, "status", "success.%s" % jobname)
 
 def time_since_last_successful_run(jobname):
     from os.path import getmtime
@@ -58,77 +33,104 @@ def time_since_last_successful_run(jobname):
     return datetime.now() - datetime.fromtimestamp(seconds_since_epoch)
 
 
-def run_command(command):
-    subprocess.check_output(command)
+def check_ping(hostname):
+    import socket
+    ClientSocket = socket.socket()
+    try:
+        ClientSocket.connect((hostname, 22))
+    except socket.error:
+        raise SkipExecution("Could not ping host %r" % hostname)
 
-def check_condition(jobname, condition):
-    time_since_last_success = time_since_last_successful_run(jobname)
+def check_hour_is_between(start, end):
+    "can also wrap around midnight, e.g. start=22, end=4"
+    now = datetime.now()
+    in_daytime_interval = start <= now.hour < end
+    in_interval_before_midnight =  end < start <= now.hour
+    in_interval_after_midnight = now.hour < end < start
+    if not any([in_daytime_interval, in_interval_before_midnight,
+               in_interval_after_midnight]):
+        raise SkipExecution("Time is not between %i:00 and %i:00" % (start, end))
 
-    def ping(hostname):
-        import socket
-        ClientSocket = socket.socket()
+def check_not_since(time_since_success, duration, time_unit):
+    if time_since_success is None:
+        # could not determine last success -> not recently successful
+        return
+    if time_since_success < timedelta(**{time_unit: duration}):
+        raise SkipExecution("Backup less than %i %s ago" % (duration, time_unit))
+
+
+class Job(object):
+    def __init__(self, name, command, condition=('and',())):
+        self.name = name
+        self.command = command
+        self.condition = condition
+        self.time_since_last_success = time_since_last_successful_run(self.name)
+
+    def check_condition(self):
         try:
-            ClientSocket.connect((hostname, 22))
-        except socket.error:
-            raise SkipExecution("Could not ping host %r" % hostname)
+            PidFile(self.name).check()
+        except PidFileError:
+            raise SkipExecution("Job already running.")
+        self._check_condition(self.condition)
 
-    def not_since(duration, time_unit):
-        if time_since_last_success is None:
-            # could not determine last success -> not recently successful
-            return
-        if time_since_last_success < timedelta(**{time_unit: duration}):
-            raise SkipExecution("Backup less than %i %s ago" % (duration, time_unit))
+    def _check_condition(self, condition):
+        def check_any(*conditions):
+            log = []
+            for c in conditions:
+                try:
+                    self._check_condition(c)
+                    return
+                except SkipExecution as ex:
+                    log.append(str(ex))
+            raise SkipExecution("All failed: %r" % log)
 
+        cmd, args = condition[0], condition[1:]
+        function = {
+            "and": lambda *conditions: [self._check_condition(c) for c in conditions],
+            "or": check_any,
+            "ping": check_ping,
+            "hour_is_between": check_hour_is_between,
+            "not_since": lambda d, t: check_not_since(self.time_since_last_success, d, t),
+        }[cmd]
+        function(*args)
 
-    def hour_is_between(start, end):
-        "can also wrap around midnight, e.g. start=22, end=4"
-        now = datetime.now()
-        in_daytime_interval = start <= now.hour < end
-        in_interval_before_midnight =  end < start <= now.hour
-        in_interval_after_midnight = now.hour < end < start
-        if not any([in_daytime_interval, in_interval_before_midnight,
-                   in_interval_after_midnight]):
-            raise SkipExecution("Time is not between %i:00 and %i:00" % (start, end))
+    def run_command(self):
+        with PidFile(self.name) as pid:
+            subprocess.check_output(self.command)
 
-    def check_any(*conditions):
-        log = []
-        for c in conditions:
-            try:
-                check_condition(jobname, c)
-                return
-            except SkipExecution as ex:
-                log.append(str(ex))
-        raise SkipExecution("All failed: %r" % log)
+    def __enter__(self, path=LOGFILE):
+        self.logger = logging.getLogger(self.name)
+        handler = RotatingFileHandler(path, maxBytes=5000, backupCount=5)
+        format_string = '%(levelname).1s %(asctime)s: ' + self.name.replace('%','%%') + ' %(message)s'
+        handler.setFormatter(logging.Formatter(format_string, datefmt=DATEFMT))
+        self.logger.addHandler(handler)
+        self.logger.setLevel(logging.DEBUG)
+        self.logger.info("start")
+        return self
 
-    def check_all(*conditions):
-        for c in conditions:
-            check_condition(jobname, c)
+    def __exit__(self, ex_type, ex, traceback):
+        if ex is None:
+            touch(success_timestamp_file(self.name))
+            self.logger.info("success")
+            urllib2.urlopen(SUCCESS_URL % self.name, timeout=10)
+        elif isinstance(ex, SkipExecution):
+            self.logger.warn("skip: %s" % ex)
+        else:
+            self.logger.error("fail: %s" % ex)
+        return True # do not throw exception again -> only gets logged, further jobs in loop get processed
 
-    cmd, args = condition[0], condition[1:]
-    print cmd, args
-    function = {
-        "and": check_all,
-        "or": check_any,
-        "ping": ping,
-        "hour_is_between": hour_is_between,
-        "not_since": not_since,
-    }[cmd]
-
-    function(*args)
 
 if __name__ == "__main__":
-    setup_logging()
-
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('--skip-condition-check', action='store_true')
-    parser.add_argument('--config-file', default=os.path.join(BACKPACK_DOTDIR, "conf.json"))
+    parser.add_argument('--config-file', default=os.path.join(BACKPACK_DOTDIR, "conf", "conf.json"))
     args = parser.parse_args()
 
     jobs = json.load(file(args.config_file))
 
-    for job in jobs:
-        with logged_call(job['name']):
+    for job_config in jobs:
+        with Job(**job_config) as job:
             if not args.skip_condition_check:
-                check_condition(job['name'], job.get('condition', ["and"]))
-            run_command(job['command'])
+                job.check_condition()
+            job.run_command()
--- borg-run.sh
+++ borg-run.sh
@@ -3,9 +3,10 @@
 set -e
 set -o pipefail
 
-BORG_CMD=borg-linux64
-PATHS="~/wip/ ~/.thunderbird/ ~/.mozilla/ ~/src"
+BORG_CMD="$HOME/bin/borg-linux64"
+PATHS="$HOME/wip/ $HOME/.thunderbird/ $HOME/.mozilla/ $HOME/src"
 
+SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BORG_CMD=${BORG_CMD:-borg}
 REPOSITORY=${REPOSITORY:-'pi@crashpi1:/media/data/borg'}
 ARCHIVE_TAG=${ARCHIVE_TAG:-"$(whoami)@$(hostname -s)"}
@@ -14,8 +15,9 @@ ARCHIVE_NAME=${ARCHIVE_NAME:-"${ARCHIVE_TAG}_${TIMESTAMP_FORMAT}"}
 
 DEFAULT_EXCLUDES="--exclude-caches --exclude '*/tmp/*' --exclude '*/nobackup/*' --exclude '*/no-backup/*' --exclude-if-present .nobackup --exclude-if-present .no-backup"
 EXCLUDES=${EXCLUDES:-${DEFAULT_EXCLUDES} ${ADDITIONAL_EXCLUDES}}
-. set_passphrase.sh   # export BORG_PASSPHRASE="..."
+. "$SRC_DIR/set_passphrase.sh"   # export BORG_PASSPHRASE="..."
 
+export BORG_LOGGING_CONF=$HOME/.backpack/conf/borg-logging.conf
 
 # if running interactively, add verbosity
 [[ -t 1 ]] && VERBOSE=${VERBOSE:-"--progress --stats --info"}
@@ -23,5 +25,5 @@ EXCLUDES=${EXCLUDES:-${DEFAULT_EXCLUDES} ${ADDITIONAL_EXCLUDES}}
 
 CMD="${BORG_CMD} create --compression lz4 ${EXCLUDES} ${VERBOSE} ${ADDITIONAL_OPTIONS} ${REPOSITORY}::${ARCHIVE_NAME} ${PATHS}"
 [[ -n "${VERBOSE}" ]] && echo "Running '$CMD'."
-eval "$CMD" | tee -a ~/.borg-run.log
+eval "$CMD" | tee ~/.backpack/log/latest-borg-run.log
 
