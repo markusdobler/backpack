3564e6d0aea2ce374d856bdc1448ea9b96f191ee
Crasphi One <pi@crashpi1>
2017-11-30 23:06:28 +0100

    crashpi1: update NAS folders to backup

--- configs/crashpi1/borg-conf.sh
+++ configs/crashpi1/borg-conf.sh
@@ -1,2 +1,2 @@
-PATHS="/media/nas_backup/sfoo/ /media/nas/pics /media/nas/0_FelixFilmeHochzeitMP4 /media/nas/BabyBackup_20160411-1020 /media/nas/HGS /media/nas/HGSFilm /media/nas/Hochzeit /media/nas/Kerstin /media/nas/Pictures /media/nas/Referendariat /media/nas/lehrwerksmaterial /media/nas/misc"
+PATHS="/media/nas_backup/sfoo/ /media/nas/Anna /media/nas/pics /media/nas/HGS /media/nas/HGSFilm /media/nas/Hochzeit /media/nas/Kerstin /media/nas/Pictures /media/nas/Referendariat /media/nas/lehrwerksmaterial /media/nas/misc"
 REPOSITORY='/media/data/borg'
